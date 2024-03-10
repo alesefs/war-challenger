a91a4a63e13c1bad1255a9f5533212fc672ebb3c
Alessandro Emanuel <alesefs@hotmail.com>
2024-03-09 23:59:18 -0300

    base swiftUI war challenger

---
+++ war-challenger/war_challengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengerApp.swift
+//  war-challenger
+//
+//  Created by Alessandro Emanuel Ferreira de Souza on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct war_challengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
